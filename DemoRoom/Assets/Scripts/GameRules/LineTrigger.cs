a53f34d43ff58e57c82e6c20872f0ca47a9b8161
Uberryans <uberryans@gmail.com>
2015-11-09 16:12:42 -0500

    Adding foul code and start of referee script

---
+++ DemoRoom/Assets/Scripts/GameRules/LineTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineTrigger : MonoBehaviour {
+
+	public GameObject controller;
+	void OnTriggerEnter(Collider col){
+		controller.GetComponent<Fouls>().LineOut();
+	}
+}
