84cc426a74d81868e53623ef477718b269726fc3
Jordan Stone <stone@drexel.edu>
2016-02-14 20:33:36 -0500

    Player walk sounds now function on networking.

--- DemoRoom/Assets/Scripts/PlayerPhysics/GoalBallPlayerMovementV1.cs
+++ DemoRoom/Assets/Scripts/PlayerPhysics/GoalBallPlayerMovementV1.cs
@@ -9,71 +9,39 @@ public class GoalBallPlayerMovementV1 : NetworkBehaviour
 {
 	//Required Components
 	private Rigidbody RB;
-	public AudioSource playerWalkSource;
-   	public AudioClip playerWalkSound;
 
 	//Movement Speed
 	public float speed = 10f;
-    public float slowSpeed = 1f;
+	public float slowSpeed = 1f;
 
-	// Modifies time between walk sounds
-	public float walkSoundMod = 1f; 
+	//Disable Horizontal when Dived
+	public bool horizontalEnabled = true;
 
-// Timer
-	// Maximum time between foot sounds
-	public float timerLimit = 1f;
-
-	// Time between walk sounds
-	private float timer = 0;
-
-// Debug Globals
-	public float avgVel; 
-	public Vector3 RBvel;
-
-    //Disable Horizontal when Dived
-    public bool horizontalEnabled = true;
-
-    /// <summary>
-    /// initializes variables that need to be used by this script.
-    /// </summary>
-    void Start ()
+	/// <summary>
+	/// initializes variables that need to be used by this script.
+	/// </summary>
+	void Start ()
 	{
 		RB = gameObject.GetComponent<Rigidbody>();
-		playerWalkSource = gameObject.GetComponentInChildren<AudioSource>();
-		playerWalkSource.clip = playerWalkSound;
 	}
 
-    /// <summary>
-    /// Gets the Horizontal and Vertical movement axis and gets their values.
-    /// Then this function increases the velocity of the player in that direction.
-    /// </summary>
-    /// <param name="Horizontal"></param>
-    /// <param name="Vertical"></param>
-    public void Move (string Horizontal, string Vertical)
+	/// <summary>
+	/// Gets the Horizontal and Vertical movement axis and gets their values.
+	/// Then this function increases the velocity of the player in that direction.
+	/// </summary>
+	/// <param name="Horizontal"></param>
+	/// <param name="Vertical"></param>
+	public void Move (string Horizontal, string Vertical)
 	{
 		float hinput = Input.GetAxis(Horizontal);
 		float vinput = Input.GetAxis(Vertical);
-        if (!horizontalEnabled)
-        {
-            RB.velocity = (gameObject.transform.up * vinput * slowSpeed);
-        }
-        else
-        {
-            RB.velocity = (gameObject.transform.right * hinput * speed) + (gameObject.transform.forward * vinput * speed);
-        }
-
-		RBvel = RB.velocity;
-
-		avgVel = (Mathf.Abs(RB.velocity.x) + Mathf.Abs(RB.velocity.z)) / 2;
-
-		if (avgVel > 0 && timer <= 0)
+		if (!horizontalEnabled)
 		{
-			timer = walkSoundMod / avgVel;
-			if (timer > timerLimit) timer = timerLimit;
-
-			playerWalkSource.PlayOneShot(playerWalkSound);
+			RB.velocity = (gameObject.transform.up * vinput * slowSpeed);
 		}
-
-		timer -= Time.deltaTime;
+		else
+		{
+			RB.velocity = (gameObject.transform.right * hinput * speed) + (gameObject.transform.forward * vinput * speed);
+		}
 	}
 }
---
+++ DemoRoom/Assets/Scripts/SoundScripts/WalkSound.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script controls the frequency of each player's feet noise based off of speed.
+/// </summary>
+
+public class WalkSound : MonoBehaviour {
+
+	// Audio Components
+	public AudioSource playerWalkSource;
+	public AudioClip playerWalkSound;
+
+	// Modifies time between walk sounds
+	public float walkSoundMod = 1f; 
+
+//Position Timer - Keeps track of change in position to update speed
+
+	// Total seconds between position update
+	private float posUpdate = 0.00005f; 
+
+	private float posTimer = 0; 
+
+	private Vector3 prevPos;
+
+// Walk Timer - Tracks time between footstep sounds
+
+	// Maximum time between foot sounds
+	public float walkTimerLimit = 0.4f;
+	// Time between walk sounds
+	private float walkTimer = 0;
+
+	// Speed var. Public for debug purposes.
+	public float avgVel; 
+
+	// Position difference. Public for debug purposes.
+	public Vector3 RBvel;
+
+	// Use this for initialization
+	void Start () {
+		playerWalkSource = gameObject.GetComponentInChildren<AudioSource>();
+		playerWalkSource.clip = playerWalkSound;
+
+		prevPos = gameObject.transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		posTimer += Time.deltaTime;
+
+		if (posTimer >= posUpdate)
+		{
+			Vector3 currPos =  gameObject.transform.position;
+
+			RBvel = currPos - prevPos;
+
+			avgVel = (Mathf.Abs(RBvel.x) + Mathf.Abs(RBvel.z)) / 2; // Networked method of finding velocity
+
+			prevPos = currPos; // Update the previous position
+			posTimer = 0;
+		}
+
+		if (avgVel > 0 && walkTimer <= 0)
+		{
+			walkTimer = walkSoundMod / avgVel;
+			if (walkTimer > walkTimerLimit) walkTimer = walkTimerLimit;
+
+			playerWalkSource.PlayOneShot(playerWalkSound);
+		}
+
+		walkTimer -= Time.deltaTime;
+	}
+
+
+}
