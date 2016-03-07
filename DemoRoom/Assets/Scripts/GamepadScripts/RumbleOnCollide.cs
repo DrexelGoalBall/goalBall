d1d0837c7050662c2779d84a37036e62706afee1
Jroosterman <jroosterman48@gmail.com>
2016-03-07 14:02:48 -0500

    fixing rumble

--- DemoRoom/Assets/Scripts/GamepadScripts/RumbleOnCollide.cs
+++ DemoRoom/Assets/Scripts/GamepadScripts/RumbleOnCollide.cs
@@ -11,6 +11,9 @@ public class RumbleOnCollide : MonoBehaviour
 
     public float rumbleStrength = .5f;
     public string collideString = "Limits";
+    private float timer = 0f;
+    public float maxTime = 1f;
+    private bool rumble = false;
 	
 	/// <summary>
     /// Detect when the player enters a collider with the tag collideString and activates the Rubmle Function.
@@ -18,7 +21,8 @@ public class RumbleOnCollide : MonoBehaviour
     /// <param name="col"></param>
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == collideString) Rumble();
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == collideString) Rumble();
     }
 
     /// <summary>
@@ -27,7 +31,26 @@ public class RumbleOnCollide : MonoBehaviour
     /// <param name="col"></param>
     void OnCollisionEnter(Collision col)
     {
+        Debug.Log(col.gameObject.tag);
         if (col.gameObject.tag == collideString) Rumble();
+    }
+
+
+    void Update()
+    {
+        if (rumble)
+        {
+            timer += Time.deltaTime;
+            GamePad.SetVibration(PlayerIndex.One, rumbleStrength, 0);
+            if (timer > maxTime)
+            {
+                rumble = false;
+            }
+        }
+        else
+        {
+            GamePad.SetVibration(PlayerIndex.One, 0, 0);
+        }
     }
 
     /// <summary>
@@ -35,6 +58,7 @@ public class RumbleOnCollide : MonoBehaviour
     /// </summary>
     public void Rumble()
     {
-        GamePad.SetVibration(PlayerIndex.One, rumbleStrength, rumbleStrength);
+        rumble = true;
+        timer = 0f;
     }
 }
