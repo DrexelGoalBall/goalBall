2a98f811b8d999c6264ec04a3f7ad7d7b0953c50
Jroosterman <jroosterman48@gmail.com>
2016-03-13 16:55:49 -0400

    Adding blindfold

---
+++ DemoRoom/Assets/Scripts/SceneScripts/Blindfold.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This is a debugging script to blind fold the player on button command.
+/// </summary>
+public class Blindfold : MonoBehaviour {
+
+    public GameObject blindfold;
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (blindfold.activeSelf) blindfold.SetActive(false);
+            else blindfold.SetActive(true);
+                
+        }
+	}
+}
