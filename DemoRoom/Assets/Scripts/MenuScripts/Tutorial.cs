802d4d54b89bb87154c67eb304b9d255f3fc85a1
Jordan Stone <j.stonecs@gmail.com>
2016-05-15 19:12:06 -0400

    Adds reading of the tutorial to the game.

--- DemoRoom/Assets/Scripts/MenuScripts/Tutorial.cs
+++ DemoRoom/Assets/Scripts/MenuScripts/Tutorial.cs
@@ -9,15 +9,44 @@ using MenuTools;
 public class Tutorial : MonoBehaviour 
 {
 	public string LeaveButton = "Cancel";
+	public string AltLeaveButton = "Start";
 
-    /// <summary>
-    ///     Updates the menu logic with the functions for this menu
-    /// </summary>
+	public AudioClip[] tutorialQueue;
+
+	private AudioSource source;
+	private int queueIndex = 0;
+
+	void Start()
+	{
+		source = GetComponent<AudioSource>();
+		source.clip = tutorialQueue[queueIndex];
+		source.Play();
+	}
+
+	/// <summary>
+	///     Updates the menu logic with the functions for this menu
+	/// </summary>
 	void Update() 
-    {
-        if (InputPlayers.player0.GetButtonDown(LeaveButton))
+	{
+		if (InputPlayers.player0.GetButtonDown(LeaveButton) || InputPlayers.player0.GetButtonDown(AltLeaveButton))
 		{
 			Application.LoadLevel("MainMenu");			
 		}
+
+		if (!source.isPlaying) // Queue up the next clip
+		{
+			queueIndex++;
+
+			if (queueIndex <= tutorialQueue.Length)
+			{
+				source.clip = tutorialQueue[queueIndex];
+				source.Play();
+			}
+			else
+			{
+				Application.LoadLevel("MainMenu");	
+			}
+		}
 	}
+
 }
